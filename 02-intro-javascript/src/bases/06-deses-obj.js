de5539d0efc91440be2abb430a857ff107b9bfb3
holomdev <holom.dev@gmail.com>
2020-08-01 17:47:43 -0500

    06 desestructuracion de objetos

---
+++ 02-intro-javascript/src/bases/06-deses-obj.js
@@ -0,0 +1,28 @@
+//desestructuracion
+//asignacion desestructurante
+const persona = {
+    nombre: 'Tony',
+    edad: 45,
+    clave: 'Ironman',
+}
+
+const { nombre, edad, clave } = persona;
+console.log(nombre);
+console.log(edad);
+console.log(clave);
+
+const useContext = ({ nombre, edad, clave, rango = "Capitan" }) => {
+    
+    return {
+        nombreClave: clave,
+        anios: edad,
+        latlng: {
+            lat: 14.1232,
+            lng: -12.3232
+        }
+    }
+}
+
+const { nombreClave, anios, latlng: {lat, lng} } = useContext(persona);
+console.log(nombreClave, anios);
+console.log(lat, lng);
