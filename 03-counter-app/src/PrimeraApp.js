d18e22d0ea9a56655ad9b09ac46553ec995fcdcb
holomdev <holomdev@gmail.com>
2020-08-07 10:02:21 -0500

    Primer componente y fragments

---
+++ 03-counter-app/src/PrimeraApp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PrimeraApp = () => {
+  return (
+    <>
+      <h1>Hola mundo</h1>
+      <p>Mi primera app</p>
+    </>
+  );
+};
+
+export default PrimeraApp;
