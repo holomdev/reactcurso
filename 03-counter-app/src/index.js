e8b1d0798ed09ac3ce30ab5c4fc2e7fd1e543a7e
holomdev <holom.dev@gmail.com>
2020-08-09 19:25:58 -0500

    componente CounterApp

--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PrimeraApp from './PrimeraApp';
+import CounterApp from './CounterApp';
 
 import './index.css';
 
 const divRoot = document.querySelector('#app');
 
-ReactDOM.render(<PrimeraApp saludo="Saludos" />, divRoot);
+ReactDOM.render(<CounterApp value={3} />, divRoot);
