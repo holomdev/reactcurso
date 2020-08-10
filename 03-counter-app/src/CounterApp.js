1405d92b3e74cad7a16b9d2e8138c2b5eb2c4278
holomdev <holom.dev@gmail.com>
2020-08-10 00:05:16 -0500

    handleReset handleSubtract

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const CounterApp = ({ value }) => {
-  const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(value);
 
   //handleAdd
   const handleAdd = (e) => {
@@ -10,11 +10,21 @@ const CounterApp = ({ value }) => {
     setCounter(counter + 1);
   };
 
+  const handleReset = (e) => {
+    setCounter(value);
+  };
+
+  const handleSubtract = (e) => {
+    setCounter(counter - 1);
+  };
+
   return (
     <>
       <h1>Counter App</h1>
       <h2>{counter}</h2>
       <button onClick={handleAdd}>+1</button>
+      <button onClick={handleReset}>Reset</button>
+      <button onClick={handleSubtract}>-1</button>
     </>
   );
 };
