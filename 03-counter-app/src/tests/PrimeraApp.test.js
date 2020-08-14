f31d3a0e8f5b9e2a07ff0f05f7328afeaa10dcf9
holomdev <holom.dev@gmail.com>
2020-08-13 22:33:51 -0500

    enzyme primera prueba

--- 03-counter-app/src/tests/PrimeraApp.test.js
+++ 03-counter-app/src/tests/PrimeraApp.test.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import PrimeraApp from '../PrimeraApp';
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('Pruebas en <PrimeraApp />', () => {
-  test('debe mostrar el mensaje "Hola soy Goku"', () => {
+  /* test('debe mostrar el mensaje "Hola soy Goku"', () => {
     const saludo = 'Hola soy Goku';
     const { getByText } = render(<PrimeraApp saludo={saludo} />);
     expect(getByText(saludo)).toBeInTheDocument();
+  }); */
+
+  test('debe mostrar <PrimeraApp /> correctamente', () => {
+    const saludo = 'Hola soy Goku';
+    const wrapper = shallow(<PrimeraApp saludo={saludo} />);
+    expect(wrapper).toMatchSnapshot();
   });
 });
