c33be7ce9617b56c0cd558b24b042bec8a777c07
holomdev <holom.dev@gmail.com>
2020-08-13 21:54:21 -0500

    pruebas en componentes

--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CounterApp from './CounterApp';
+/* import CounterApp from './CounterApp'; */
+import PrimeraApp from './PrimeraApp';
 
 import './index.css';
 
 const divRoot = document.querySelector('#app');
 
-ReactDOM.render(<CounterApp value={3} />, divRoot);
+/* ReactDOM.render(<CounterApp value={3} />, divRoot); */
+ReactDOM.render(<PrimeraApp saludo="Hello" />, divRoot);
