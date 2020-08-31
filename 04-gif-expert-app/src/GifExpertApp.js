b91fda8b897a34b71eb31d0e9b4a5a94b3925214
holomdev <holom.dev@gmail.com>
2020-08-30 21:17:34 -0500

    Creando una lista de categorias

--- 04-gif-expert-app/src/GifExpertApp.js
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const GifExpertApp = () => {
+  //const categories = ['One punc', 'Samurai X', 'Dragon Ball'];
+
+  const [categories, setcategories] = useState([
+    'One punc',
+    'Samurai X',
+    'Dragon Ball',
+  ]);
+
+  const handleAdd = () => {
+    setcategories([...categories, 'Jojos']);
+  };
+
   return (
     <>
       <h2>GifExpertApp</h2>
       <hr />
+
+      <button onClick={handleAdd}>Agregar</button>
+
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}> {category}</li>;
+        })}
+      </ol>
     </>
   );
 };
